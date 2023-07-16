a0cab52c63311521236d669b498aad2f2e46e9e8
yadevom <yaormu@gmail.com>
2023-07-16 17:23:09 -0500

    Renderizar componentes dentro de componentes Se crea componente HeaderComponent Que recibe 3 parámetros, 2 obligatiorios y 1 opcional El opcional debe ser un componente

--- ReactJS_Typescript_MaterialUI_codrr/marketplace/src/pages/home/index.tsx
+++ ReactJS_Typescript_MaterialUI_codrr/marketplace/src/pages/home/index.tsx
@@ -1,12 +1,20 @@
 import { Button, Container } from "@mui/material";
+import { HeaderComponent } from "../../components";
 
 export const HomePage: React.FC = () => {
 
  return (
-    <Container sx={{ mt: 9 }} maxWidth="xl">
-      <Button fullWidth variant="contained">
-        Componente Pagina Home
-      </Button>
+    <Container maxWidth="xl">
+      <HeaderComponent
+        title = "Mundo ReactJS"
+        description="Biblioteca para crear interfaces de usuarios"
+        element={
+          <Button fullWidth variant="contained">
+            🐣
+          </Button>}
+      >
+
+      </HeaderComponent>
     </Container>
   );
 };
