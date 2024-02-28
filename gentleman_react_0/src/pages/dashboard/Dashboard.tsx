6edc42215cad2f58d4ee5952b99aa000e6417017
yadevom <yaormu@gmail.com>
2024-02-28 07:16:46 -0500

    cambiso

--- gentleman_react_0/src/pages/dashboard/Dashboard.tsx
+++
@@ -1,2 +0,0 @@
-// Dashboard va a ser un contenedor que va a tener desde estructura general
-// Seria el que maneja las condiciones, como cuando se muestre un formulario
--- gentleman_react_0/src/pages/dashboard/components/DashboardForm.tsx
+++
@@ -1,3 +0,0 @@
-// Aquí se tendria la lógica puntual del formulario
-// Ya que sera un componente que solo maneje un formulario
-
